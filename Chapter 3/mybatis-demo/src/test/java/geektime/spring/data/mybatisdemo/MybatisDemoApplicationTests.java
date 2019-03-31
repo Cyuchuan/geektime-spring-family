02da065b334b41a2ae571c8284885012d9ccd125
chenyuchuan <chuan_juan@163.com>
2019-03-31 14:33:58 +0800

    将h2修改为test，并添加相应的application-test

--- Chapter 3/mybatis-demo/src/test/java/geektime/spring/data/mybatisdemo/MybatisDemoApplicationTests.java
+++ Chapter 3/mybatis-demo/src/test/java/geektime/spring/data/mybatisdemo/MybatisDemoApplicationTests.java
@@ -1,17 +1,43 @@
 package geektime.spring.data.mybatisdemo;
 
+import geektime.spring.data.mybatisdemo.mapper.CoffeeMapper;
+import geektime.spring.data.mybatisdemo.model.Coffee;
+import lombok.extern.slf4j.Slf4j;
+import org.joda.money.CurrencyUnit;
+import org.joda.money.Money;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Sql("classpath:schemaq.sql")
+@Slf4j
 public class MybatisDemoApplicationTests {
+    @Autowired
+    private ApplicationContext applicationContext;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private CoffeeMapper coffeeMapper;
+
+    @Test
+    public void contextLoads() {
+        DataSource bean = applicationContext.getBean(DataSource.class);
+
+        Coffee c = Coffee.builder().name("espresso")
+                .price(Money.of(CurrencyUnit.of("CNY"), 20.0)).build();
+        Long id = coffeeMapper.save(c);
+        log.info("Coffee {} => {}", id, c);
+
+        c = coffeeMapper.findById(id);
+        log.info("Coffee {}", c);
+    }
 
 }
 
